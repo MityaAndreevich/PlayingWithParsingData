67b9985f83d6fde8fc8bae64343faceab51e07bd
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-25 19:03:31 +0300

    some work with collection view, segue identifiers, cell view

--- PlayingWithParsingData/ViewControllers/MainViewController.swift
+++ PlayingWithParsingData/ViewControllers/MainViewController.swift
@@ -20,13 +20,7 @@ class MainViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-
-        // Do any additional setup after loading the view.
+      
     }
 
     /*
@@ -41,21 +35,15 @@ class MainViewController: UICollectionViewController {
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
     
-        // Configure the cell
+       
     
         return cell
     }
--- PlayingWithParsingData/ViewControllers/UserActionCell.swift
+++ PlayingWithParsingData/ViewControllers/UserActionCell.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class UserActionCell: UICollectionViewCell {
     
+    @IBOutlet var userActionLabel: UILabel!
 }
