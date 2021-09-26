3892453f2edd458409b9d099b41f87c711591c0d
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-26 17:41:54 +0300

    sending loading of images to global thread in order to get rid of main thread overloading and freezes in process

--- PlayingWithParsingData/ViewControllers/CoursesCell.swift
+++ PlayingWithParsingData/ViewControllers/CoursesCell.swift
@@ -18,10 +18,12 @@ class CoursesCell: UITableViewCell {
         coursesName.text = course.name
         coursesLessons.text = "Number of lessons: \(course.number_of_lessons ?? 0)"
         coursesTests.text = "Number of tests: \(course.number_of_tests ?? 0)"
-        guard let url = URL(string: course.imageUrl ?? "") else { return }
-        guard let imageData = try? Data(contentsOf: url) else { return }
-        DispatchQueue.main.async {
-            self.coursesImage.image = UIImage(data: imageData)
+        DispatchQueue.global().async {
+            guard let url = URL(string: course.imageUrl ?? "") else { return }
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.coursesImage.image = UIImage(data: imageData)
+            }
         }
     }
 }
